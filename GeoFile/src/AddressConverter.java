414546e4305319fc308a0c4bb03ac3c21d9d6b43
krunalbhatt9 <krunal.bhatt9@gmail.com>
2015-03-28 19:47:40 +0530

    Geopositioninng catalog

--- GeoFile/src/AddressConverter.java
+++ GeoFile/src/AddressConverter.java
@@ -48,61 +48,7 @@ public class AddressConverter {
   
 
  }
- /*
- public GoogleResponse convertFromLatLong(String latlongString) throws IOException {
 
-  
-   * Create an java.net.URL object by passing the request URL in
-   * constructor. Here you can see I am converting the fullAddress String
-   * in UTF-8 format. You will get Exception if you don't convert your
-   * address in UTF-8 format. Perhaps google loves UTF-8 format. :) In
-   * parameter we also need to pass "sensor" parameter. sensor (required
-   * parameter) — Indicates whether or not the geocoding request comes
-   * from a device with a location sensor. This value must be either true
-   * or false.
-   
-  URL url = new URL(URL + "?latlng=" + URLEncoder.encode(latlongString, "UTF-8") + "&sensor=false");
-  // Open the Connection
-  URLConnection conn = url.openConnection();
-
-  InputStream in = conn.getInputStream() ;
-  ObjectMapper mapper = new ObjectMapper();
-  GoogleResponse response = (GoogleResponse)mapper.readValue(in,GoogleResponse.class);
-  in.close();
-  return response;
-  
-
- }
-*/
- /*
- public static void main(String[] args) throws IOException {
-  
-	 
-	 
-	 
-	 
-  GoogleResponse res = new AddressConverter().convertToLatLong("M.S.Ramaiah, Bangalore, India");
-  if(res.getStatus().equals("OK"))
-  {
-   for(Result result : res.getResults())
-   {
-    System.out.println("Lattitude of address is :"  +result.getGeometry().getLocation().getLat());
-    System.out.println("Longitude of address is :" + result.getGeometry().getLocation().getLng());
-    System.out.println("Location is " + result.getGeometry().getLocation_type());
-   }
-  }
-  else
-  {
-   System.out.println(res.getStatus());
-  }
-  
-  
-  
-  
-  
-  
-  
- }
- */
-
+ 
+ 
 }
